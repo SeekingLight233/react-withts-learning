675726bfb95e3f68f7db15bd2dd9dc75966ed85d
Jason <690163223@qq.com>
2020-05-29 09:03:32 +0800

    feat: useState1

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const LikeButton: React.FC = () => {
   //es6的解构赋值，不是ts专门的语法
   const [like, setLike] = useState(0) //The useState() returns a stateful value, and a function to update it.
   const [on, setOn] = useState(true)
+  useEffect(() => {
+    //组件渲染完成之后，传进来的func会被执行
+    document.title = `点击了${like}次`
+  })
   return (
     <div>
       <button
